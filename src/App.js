29b9f27597e3604615c48bbdca1bbcb775c81ac8
Reece Peardon <peardon.reece@gmail.com>
2024-12-12 23:40:55 -0800

    set up project and began nav bar development

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <Router>
+      <Nav />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../assets/logo.png'
+
+function Nav() {
+  return (
+    <nav>
+      <img src={Logo} alt='YGO_Logo'></img>
+    </nav>
+  );
+}
+
+export default Nav;
