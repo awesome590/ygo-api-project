5dec38b1622c4ccaa0c8275b0fdae76c41b6b651
Reece Peardon <peardon.reece@gmail.com>
2025-01-24 20:19:15 -0800

    continued to set up the nav bar and get the css styling done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from 'react'
+import {Link} from 'react-router-dom'
 import Logo from '../assets/logo.png'
 
 function Nav() {
   return (
     <nav>
-      <img src={Logo} alt='YGO_Logo'></img>
+      <div className="nav__container">
+        <img src={Logo} alt='YGO_Logo' className='logo'></img>
+        <ul className='nav__links'>
+          <li className='nav__list'>
+            <Link to='/' className='nav__link'>Home</Link>
+          </li>
+          <li className="nav__list">
+            <Link to='/' className='nav__link'>Cards</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
